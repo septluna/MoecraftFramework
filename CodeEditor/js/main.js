c6b2d53898965ecd88f477770ca25d54e1d2f362
灵长目 <lizheminglzm@gmail.com>
2016-02-23 22:08:15 +0800

    temp

--- CodeEditor/js/main.js
+++ CodeEditor/js/main.js
@@ -1,7 +1,45 @@
-﻿function highLight(){
+﻿//var fso = new ActiveXObject(Scripting.FileSystemObject);
+//var file = fso.createtextfile;
+var editWindow = $('#edit_window');
+
+
+/**
+ * 代码高亮
+ */
+
+function highLight(){
 	
 }
+
+
+
+/**
+ * 查错
+ */
 
 function syntaxError(){
 	
 }
+
+
+
+/**
+ * 自动补全代码
+ */
+
+function autoComplete (keyCode){
+	switch (keyCode){
+		case ''
+	}
+}
+
+
+
+/**
+ * 注册事件
+ */
+
+editWindow.on('keydown', function(event){
+	event = event || window.event;
+	autoComplete(event.keyCode)
+})
