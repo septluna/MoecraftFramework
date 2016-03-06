3eef5b5f2082fd114db287f0436e7573e8d84399
灵长目 <lizheminglzm@gmail.com>
2016-03-07 00:53:40 +0800

    优化代码提示
    支持中文输入法一次性输入多个字符的情况（之前仅判断前一个字符是否存为关键字的一部分，在特定条件下会出现误判）
    代码提示框的动画效果

--- CodeEditor/js/main.js
+++ CodeEditor/js/main.js
@@ -36,35 +36,17 @@ for (var i = 0; i < funcNum; i++){
 }
 var hintList = [];
 var findInList = false;
+var lastCurPos;
+
 
 function autoComplete() {
-
-//	//在行首按下"<"
-//	if (getForwardWord(2) == '\n<' || (getCursorPos() == 1 && getForwardWord() == '<')) {
-//		if (listenInput == false) {
-//			listenInput = true;
-//			window.startPos = getCursorPos();
-//		}
-//	}
-//
-//	//监听输入
-//	if (listenInput == true) {
-//		for (var i = 0; i < funcNum; i++) {
-//			//在第一位找到了关键字
-//			if (getForwardWord(getCursorPos() - startPos).indexOf(rules.function[i].name) == 0) {
-//				for (var j = 0; j < rules.function[i].attr.length; j++) {
-//					insertStr(' ' + rules.function[i].attr[j] + '=""');
-//				}
-//				insertStr('>');
-//
-//				listenInput = false;
-//			}
-//		}
-//	}
+	
+	//设定要截取的字符数量（针对中文输入法会一次性输出多个字符的情况）
+	var wordLen = getCursorPos() - lastCurPos; 
 	
 	
-	//前一个字符存在于关键字中
-	if (getForwardWord().length > 0 && functionsName.indexOf(getForwardWord()) != -1 && !findInList){
+	//第一个字符存在于关键字中
+	if (getForwardWord().length > 0 && functionsName.indexOf(getForwardWord(wordLen)) > -1 && !findInList){
 		for (var i = 0, j = 0; i < funcNum; i++) {
 			if (rules.function[i].name.indexOf(getForwardWord()) > -1){
 				hintList[j] = rules.function[i].name;
@@ -74,23 +56,37 @@ function autoComplete() {
 		findInList = true;
 	}
 	
+	
 	//删除提示列表中不匹配的项
 	if (findInList){
 		for (var i = 0; i < hintList.length; i++) {
-			if (hintList[i].indexOf(getForwardWord()) == -1){
+			if (hintList[i].indexOf(getForwardWord(wordLen)) == -1){
 				hintList.splice(i, 1);
 				i = -1;
 			}
 		}
 	}
 
+
+
 	//显示提示框
 	if (hintList.length > 0){
-		$('#code_hinting').css({
-			'display': 'block',
+		if (getCurCordinate().left + $('#code_hinting').width() - 10 > $(window).width() - ($(window).width() - editWindow.width()) / 2){
+			$('#code_hinting').css({
+			'visibility': 'visible',
+			'opacity': '1',
+			'top': getCurCordinate().top + 25,
+			'left': $(window).width() - ($(window).width() - editWindow.width()) / 2 - $('#code_hinting').width() - 10
+			});
+		}
+		else {
+			$('#code_hinting').css({
+			'visibility': 'visible',
+			'opacity': '1',
 			'top': getCurCordinate().top + 25,
 			'left': getCurCordinate().left - 20
-		});
+			});
+		}
 		
 		var hintVal = '';
 		for (var i = 0; i < hintList.length; i++){
@@ -102,7 +98,7 @@ function autoComplete() {
 	// 无匹配时隐藏提示框
 	else {
 		$('#code_hinting').css(
-			'display', 'none'
+			'visibility', 'hidden'
 		);
 		findInList = false;
 	}
@@ -127,9 +123,18 @@ function autoComplete() {
 		//匹配到关键字
 		if (getForwardWord(rules.function[i].name.length).indexOf(rules.function[i].name) != -1 && !isHintShow){
 			isHintShow = true;
-
 		}
 	}
+	
+	
+	
+	//记录光标位置
+	lastCurPos = getCursorPos();
+
+	
+	/**
+	 * TODO: 鼠标点击其他位置时，隐藏提示框、清除光标位置记录、findInList归false、清空列表…… 
+	 */
 }
 
 
